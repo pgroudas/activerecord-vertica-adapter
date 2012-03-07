ccdf769b8129af8b120cad9a270384c490073676
Paul Groudas <me@paulgroudas.com>
2012-03-06 21:59:34 -0500

    PJ: First stab at actual driver

--- lib/arjdbc/vertica/adapter.rb
+++ lib/arjdbc/vertica/adapter.rb
@@ -1,4 +1,8 @@
-require 'arjdbc/mssql/tsql_helper'
+module ActiveRecord
+ module ConnectionAdapters
+  AR_TO_JDBC_TYPES[:string] << lambda {|r| r['type_name'] =~ /varchar$/i} # For Vertica
+ end
+end
 
 module ::ArJdbc
   module Vertica
--- lib/arjdbc/vertica/connection_methods.rb
+++ lib/arjdbc/vertica/connection_methods.rb
@@ -1,10 +1,10 @@
 class ActiveRecord::Base
   class << self
-    #def cachedb_connection( config )
-      #config[:port] ||= 1972
-      #config[:url] ||= "jdbc:Cache://#{config[:host]}:#{config[:port]}/#{ config[:database]}"
-      #config[:driver] ||= "com.intersys.jdbc.CacheDriver"
-      #jdbc_connection(config)
-    #end
+    def vertica_connection( config )
+      config[:port] || 5433
+      config[:url] ||= "jdbc:vertica://#{config[:host]}:#{config[:port]}/#{ config[:database]}"
+      config[:driver] ||= "com.vertica.jdbc.Driver"
+      jdbc_connection(config)
+    end
   end
 end
